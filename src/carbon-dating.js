225aadb4f4e7837c3a9a891bbfc9faeb2b9bd25e
toxter <tyutyunnik.serg@gmail.com>
2021-03-30 22:06:14 +0300

    feat: complete carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(inputString) {
+  // Check for correct input
+  if ((typeof inputString == "string") || (inputString > 15) || (inputString < 1) || (inputString != '')) {
+    return false;
+  }
+  // Returned rounded ceil calculated data
+  return Math.ceil((Math.log(MODERN_ACTIVITY / inputString)) / (0.693 / HALF_LIFE_PERIOD));
 };
